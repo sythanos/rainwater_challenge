62b029c59e7aa05f5e1e90ed77f648cadcdc55d3
Urban Avsec <urban.avsec@outlook.com>
2020-08-26 14:50:27 +0200

    Added Sides and Comparison

--- src/env.rs
+++ src/env.rs
@@ -1,3 +1,5 @@
+use std::f32;
+
 pub struct Environment {
     columns: Vec<Column>,
     rain: Vec<f32>,
@@ -5,6 +7,7 @@ pub struct Environment {
 
 impl Environment {
     /// Constructs a new `Environment`
+    #[allow(dead_code)]
     pub fn new(columns: Vec<u32>) -> Self {
         Self {
             rain: vec![0.; columns.len()],
@@ -13,19 +16,32 @@ impl Environment {
                 .map(|height| Column::new(*height as f32))
                 .collect::<Vec<Column>>(),
         }
+        .add_sides()
+    }
+
+    fn add_sides(mut self) -> Self {
+        let mut columns = vec![Column::new(f32::MAX)];
+        columns.append(&mut self.columns);
+        columns.push(Column::new(f32::MAX));
+
+        self.columns = columns;
+
+        self
     }
 
     /// Returns the Water Level of the environment at position `pos`.
+    #[allow(dead_code)]
     pub fn water_level(&self, pos: usize) -> f32 {
         self.columns[pos].water_level()
     }
 
     /// Accepts the number of hours it has rain and mutate the environment to
     /// its endstate.
+    #[allow(dead_code)]
     pub fn rain(&mut self, rain_hours: f32) {
         self.rain = vec![rain_hours; self.columns.len()];
 
-        self.flow(0);
+        self.flow(1);
     }
 
     fn new_rain(&mut self, curr_pos: usize) -> f32 {
@@ -37,8 +53,12 @@ impl Environment {
     }
 
     fn flow(&mut self, curr_pos: usize) {
-        if curr_pos == self.columns.len() {
+        if curr_pos >= self.columns.len() - 1 {
             return;
+        }
+
+        if self.columns[curr_pos] > self.columns[curr_pos + 1] {
+            unimplemented!();
         }
 
         let rain_water = self.new_rain(curr_pos);
@@ -68,6 +88,18 @@ impl Column {
     }
 }
 
+impl PartialEq for Column {
+    fn eq(&self, other: &Self) -> bool {
+        self.water_level() == other.water_level()
+    }
+}
+
+impl PartialOrd for Column {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.water_level().partial_cmp(&other.water_level())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -77,21 +109,21 @@ mod tests {
     fn test_1_cols_1_water() {
         let mut env = Environment::new(vec![1]);
         env.rain(1.0);
-        approx_eq!(env.water_level(0), 2.0)
+        approx_eq!(env.water_level(1), 2.0)
     }
 
     #[test]
     fn test_11_cols_1_water() {
         let mut env = Environment::new(vec![1, 1]);
         env.rain(1.0);
-        approx_eq!(env.water_level(0), 2.0)
+        approx_eq!(env.water_level(1), 2.0)
     }
 
-    // #[test]
-    // fn test_21_cols_1_water() {
-    //     let mut env = Environment::new(vec![2, 1]);
-    //     env.rain(1.0);
-    //     approx_eq!(env.water_level(0), 2.0);
-    //     approx_eq!(env.water_level(1), 2.0);
-    // }
+    #[test]
+    fn test_21_cols_1_water() {
+        let mut env = Environment::new(vec![2, 1]);
+        env.rain(1.0);
+        approx_eq!(env.water_level(1), 2.0);
+        approx_eq!(env.water_level(2), 2.0);
+    }
 }
